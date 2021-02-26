ea7a839e1fab53ea8e0ac4b7ef37b1f7713da255
jainlajolla@gmail.com <jainlajolla@gmail.com>
2021-02-25 23:05:16 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,6 +1,6 @@
 function initMap() {
 	// add your code here
-	L.mapquest.key = 'KEY'
+	L.mapquest.key = 't4o8LCjS2Y9FSknHDIeP4bBAq9y6VewD'
 	var map = L.mapquest.map('map', {
 	center: [32.88, -117.236],
 	layers: L.mapquest.tileLayer('map'),
